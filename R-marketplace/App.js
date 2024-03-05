95346681e0b62c546c3782fca403d7388f29f47f
py1u <peter.lu723@gmail.com>
2024-03-04 21:39:05 -0800

    added props name to display different users in waitlist

--- R-marketplace/App.js
+++ R-marketplace/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Text, TextInput, View} from 'react-native';
 
-const Scottie = () => {
-  const username = "ScottieBear";
+const Scottie = props => {
   return (
     <View>
-      <Text>Hello, I am {username}!</Text> 
+      <Text>Hello, I am {props.name}!</Text> 
     </View>
   );
 };
@@ -14,9 +13,9 @@ const introList = () => {
   return (
     <View>
       <Text>The wait list be like:</Text>
-      <Scottie />
-      <Scottie />
-      <Scottie />
+      <Scottie name = "Peter" />
+      <Scottie name = "Pete" />
+      <Scottie name = "Petah" />
       
     </View>
   )
